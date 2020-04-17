890c87c9e7d3341fa1ac51997c6bc9844c8c7dd1
Galdineris <galdineris@gmail.com>
2020-04-17 13:57:23 -0300

    Initial controller setup replacing the SwiftUI configuration

---
+++ WhoSaidThatAtTheOffice/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  WhoSaidThatAtTheOffice
+//
+//  Created by Rafael Galdino on 17/04/20.
+//  Copyright © 2020 Rafael Galdino. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
